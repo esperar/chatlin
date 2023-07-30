ee637372be9823c710f4684ff00661c043287dbe
aesperer <piyrw9754@gmail.com>
2023-07-30 16:34:01 +0900

    reservation controller

--- src/main/kotlin/esperer/chatlin/reservation/controller/ReservationController.kt
+++ src/main/kotlin/esperer/chatlin/reservation/controller/ReservationController.kt
@@ -1,8 +1,41 @@
 package esperer.chatlin.reservation.controller
 
+import esperer.chatlin.chat.listener.WebSocketEventListener
+import esperer.chatlin.reservation.data.Seat
+import esperer.chatlin.reservation.persistence.SeatReservationRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.messaging.simp.SimpMessageSendingOperations
+import org.springframework.messaging.simp.SimpMessagingTemplate
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import kotlin.math.log
 
 @RestController
-class ReservationController {
+class ReservationController(
+    private val seatReservationRepository: SeatReservationRepository,
+    private val simpMessagingTemplate: SimpMessagingTemplate
+) {
+
+    private val logger: Logger = LoggerFactory.getLogger(WebSocketEventListener::class.java)
+
+    @RequestMapping("/api/v1/reservation/{id}")
+    fun reservationSeat(@PathVariable id: Int) {
+        try {
+            seatReservationRepository.reserveSeat(id)
+            simpMessagingTemplate.convertAndSend("/seat", Seat(id, true))
+            logger.info("좌석번호 $id 예약되었습니다.")
+        } catch (e: Exception){
+            throw RuntimeException(e)
+        }
+    }
+
+    @RequestMapping("/api/v1/reservation/cancel/{id}")
+    fun cancelReservation(@PathVariable("id") id: Int){
+        seatReservationRepository.cancelReservation(id)
+        simpMessagingTemplate.convertAndSend("/seat", Seat(id, false))
+        println("좌석번호 $id 예약 취소 되었습니다")
+    }
 
 }
---
+++ src/main/kotlin/esperer/chatlin/reservation/data/Seat.kt
@@ -0,0 +1,6 @@
+package esperer.chatlin.reservation.data
+
+data class Seat(
+    val id: Int,
+    val hasReservation: Boolean
+)
