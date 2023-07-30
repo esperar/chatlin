5dcb10abb88994fa77165dbbfa5138c0c60e024a
aesperer <piyrw9754@gmail.com>
2023-07-30 09:23:18 +0900

    chat models

---
+++ src/main/kotlin/esperer/chatlin/chat/model/ChatMessage.kt
@@ -0,0 +1,9 @@
+package esperer.chatlin.chat.model
+
+data class ChatMessage(
+    var type: String,
+    var content: String,
+    var sender: String
+)
+
+
---
+++ src/main/kotlin/esperer/chatlin/chat/model/MessageType.kt
@@ -0,0 +1,7 @@
+package esperer.chatlin.chat.model
+
+enum class MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
